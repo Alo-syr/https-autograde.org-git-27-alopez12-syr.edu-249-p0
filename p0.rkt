093918158ee4b976b79da7baaec6482085649cf7
Alo-syr <125357859+Alo-syr@users.noreply.github.com>
2023-09-25 23:51:22 -0400

    Update p0.rkt
    final

--- p0.rkt
+++ p0.rkt
@@ -70,34 +70,76 @@
 ;;; If player ('X or 'O) want to make a move, check whether it's this
 ;;; player's turn and the position on the board is empty ('E)
 (define (valid-move? board row col player)
-  ; Check if it's the player's turn
+  
+  ; Check if it's the player's turn using functions 3.2
   (define current-player (next-player board))
   (define is-players-turn (equal? current-player player))
   
-  ; Calculate the board size
-  (define board-size (sqrt (length board)))
+  (define boardSize (sqrt (length board)))
   
-  ; Check if the specified position is within bounds
-  (define in-bounds? (and (<= 0 row (- board-size 1))
-                          (<= 0 col (- board-size 1))))
+  ; Check if the specified position is within bounds by comparison with edges
+  (define inBounds? (and (<= 0 row (- boardSize 1))
+                          (<= 0 col (- boardSize 1))))
   
-  ; Helper function to check if a position is empty
-  (define (is-empty? lst r c)
-    (equal? (list-ref lst (+ (* r board-size) c)) 'E))
+  ; Checks if a position is empty with list-ref 
+  (define (emptyCheck? lst row col)
+    (equal? (list-ref lst (+ (* row boardSize) col)) 'E))
   
   ; Check if the position is within bounds and empty
-  (if (and is-players-turn in-bounds?)
-      (is-empty? board row col)
-      #f)) ; Return false if any condition is not met
+  (if (and is-players-turn inBounds?)
+      (emptyCheck? board row col)
+      #f))
 
 ;;; To make a move, replace the position at row col to player ('X or 'O)
 ;;; We have implemented this for you--you don't have to write it
+;;; (just a side note I first saw this and didn't read the comments
+;;; I was so scared for a solid 5 minutes lmao)
 (define (make-move board row col player)
   (list-set board (+ (* (sqrt (length board)) row) col) player))
 
 ;;; To determine whether there is a winner?
 (define (winner? board)
-  'todo)
+
+  ;Will make checking arrays via divide and conquer possible
+  (define boardSize (sqrt (length board)))
+  
+  ; Checks for row win by indexing through each elem in sub array
+  ; and compares first elem to rest of sub array (only for single row)
+  (define (checkRow row)
+    (if (equal? (car row) (car (cdr row)))
+        (if (= (length row) 1)
+            (car row) 
+            (checkRow (cdr row)))
+        #f))
+  ; Checks all rows by using checkRow recursively
+  (define (checkRows row)
+    (if (null? row)
+        #f
+        (if (checkRows (take (car row) boardSize)) ;disclaimer: I used chatgpt for inspiration here
+            (car (car row)) 
+            (checkRows (cdr row)))))
+  
+  (define rowWinner (checkRows board))
+  
+  ; Checks the top left to bottom right diagonal by indexing through each first char on top left then decreases board size by one
+  (define (leftDiagonal board boardSize)
+    (if (= boardSize 0)
+        '()
+        (cons (car (car board)) (leftDiagonal (cdr (map cdr board)) (- boardSize 1)))))
+  
+  ;Checks the top right to bottom left diagonal the same means, but in reverse which checks top right then decreases
+  (define (rightDiagonal board boardSize)
+    (if (= boardSize 0)
+        '()
+        (cons (car (reverse (car board))) (rightDiagonal (cdr (map cdr board)) (- boardSize 1)))))
+
+   ; Check for a win in diagonals (ran out of time :( )
+  (define leftdiagonalRow (leftDiagonal board boardSize))
+ 
+   (if rowWinner
+      rowWinner
+      #F)
+  )
 
 ;;; OPTIONAL -- extra practice, exploring symbolic AI
 ;;; Please ping me if you *do* write a solution -- Kris
